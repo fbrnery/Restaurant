07ec9fcfd979a558b2657556786843a474dbfbdb
Fabricio <fbrnery@gmail.com>
2015-08-21 23:37:13 -0300

    Create minitest

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module Restaurant
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :minitest, spec: true
+      g.helper false
+      g.assets false
+    end
   end
 end
